c085750f0fd9233324cfffb06b438651089562cd
Shiven <shiven.sharma5111@gmail.com>
2025-09-26 18:00:37 +0000

    Math Operaters are cool

---
+++ Lesson03_math.py
@@ -0,0 +1,43 @@
+# MATH OPERATER CONCEPTS: +, -, *, /, //, %, **
+
+add = 7 + 2
+print("Sum: ", add)
+
+subtract = 7 - 2
+print("Difference: ", subtract)
+
+multiply = 7 * 2
+print("Product: ", multiply)
+
+float_divide = 7 / 2
+print("Float Quotient: ", float_divide)
+
+integer_divide = 7 // 2
+print("Integer Quotient: ", integer_divide)
+
+modulus = 7 % 2
+print("Remainder: ", modulus)
+
+exponent = 7 ** 2
+print("Exponent: ", exponent)
+
+print(2 + 2 != 10 - 6) # conditionals
+
+
+# Order of Operation
+
+result1 = (2 + 3) * 4
+print("Result 1: ", result1)
+
+result2 = 2 ** 3 * 4
+print("Result 2: ", result2)
+
+result3 = 20 / 5 * 2
+print("Result 3: ", result3)
+
+result4 = 10 - 2 + 3
+print("Result 4: ", result4)
+
+result5 = 5 + 2 ** 3 * (4 - 3)
+print("Result 5: ", result5)
+
