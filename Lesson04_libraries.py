d58a81a48f892aa12443724fa6bfbc066e8e6d61
Shiven <shiven.sharma5111@gmail.com>
2025-09-30 16:26:55 +0000

    Random exp challenge

--- Lesson04_libraries.py
+++ Lesson04_libraries.py
@@ -7,3 +7,14 @@ print("Round Down to an Integer: ", math.floor(6.8))
 print("Power: ", math.pow(2, 5))
 print("Pi", math.pi)
 
+
+seed = 798.0982
+
+seed /= 4.9
+
+seed -= 279.023
+
+seed *= 97.0932
+
+print(seed)
+
