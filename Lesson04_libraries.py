4df96ee1361a0c9a625c7a1b91d287b55e2d42da
Shiven <shiven.sharma5111@gmail.com>
2025-09-30 15:53:59 +0000

    Math Examples

---
+++ Lesson04_libraries.py
@@ -0,0 +1,9 @@
+import math
+import random
+
+print("Square Root: ", math.sqrt(25))
+print("Round Up to an Integer: ", math.ceil(3.4))
+print("Round Down to an Integer: ", math.floor(6.8))
+print("Power: ", math.pow(2, 5))
+print("Pi", math.pi)
+
