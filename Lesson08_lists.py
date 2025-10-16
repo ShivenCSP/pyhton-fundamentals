ec681e3cdd3c9a241bc99de4011a091fdad86b4d
Shiven <shiven.sharma5111@gmail.com>
2025-10-16 18:25:11 +0000

    get better kid

--- Lesson08_lists.py
+++ Lesson08_lists.py
@@ -1,50 +1,131 @@
-# LISTS IN PYTHON
-# Lists store multiple values in one variable.
-# They are ordered, mutable (changeable), and allow duplicates.
+# # LISTS IN PYTHON
+# # Lists store multiple values in one variable.
+# # They are ordered, mutable (changeable), and allow duplicates.
 
-print()
-print("--- Lists in Python ---")
+# print()
+# print("--- Lists in Python ---")
 
-animals = ["donkey", "pangolin", "blobfish"]
-numbers = [2, 4, 6, 8, 10]
-mixed = ["piffle", 42, True, 9.99]
+# #initialize a list with brackets:
+# empty_list = [] 
+# empty_list.append("Thing")
+# print(empty_list)
 
-print(animals)
-print(numbers)
-print(mixed)
+# animals = ["donkey", "pangolin", "blobfish"]
+# numbers = [2, 4, 6, 8, 10]
+# mixed = ["piffle", 42, True, 9.99]
 
-print()
-print()
-print()
-print("--- Indexing: how to access the elements of a list ---")
-print("First Element:", animals[0])
-print("Second Element:", animals[1])
-print("Last element:", animals[-1] )
+# print(animals)
+# print(numbers)
+# print(mixed)
 
-print()
-print("--- Modifying Lists ---")
-animals[0] = "babirusa"
-print(animals)
+# print()
+# print()
+# print()
+# print("--- Indexing: how to access the elements of a list ---")
+# print("First Element:", animals[0])
+# print("Second Element:", animals[1])
+# print("Last element:", animals[-1] )
 
-animals.append("glass frog")
-print(animals)
+# print()
+# print("--- Modifying Lists ---")
+# animals[0] = "babirusa"
+# print(animals)
 
-# add elemnt at end:
-animals.append("glass frog")
-print(animals)
+# animal_to_replace = animals.index("pangolin")
+# print(animal_to_replace)
+# animals[animal_to_replace] = "seamonkey"
+# print(animals)
 
-# insert element at specific index
-animals.insert(3, "aye-aye")
-print("Inserted one animal:", animals)
-animals.insert(1, "camel")
-print("Inserted another animal:", animals)
+# # add elemnt at end:
+# animals.append("glass frog")
+# print(animals)
 
-animals.remove("babirusa")
-print(animals)
+# # insert element at specific index
+# animals.insert(3, "aye-aye")
+# print("Inserted one animal:", animals)
+# animals.insert(1, "camel")
+# print("Inserted another animal:", animals)
 
-last_animal = animals.pop()
-print("Removed animal:", last_animal)
-print("Remaining animals:", animals)
+# animals.remove("babirusa")
+# print(animals)
 
+# last_animal = animals.pop()
+# print("Removed animal:", last_animal)
+# print("Remaining animals:", animals)
 
+# print()
+# print("--- Useful List Functions ---")
 
+# nums = [3, 7, 1, 9, 4, 2, 5, 8, 6, 0]
+# print("Original Numbers:", nums)
+# print("Length of the list:", len(nums))
+# print("Min:", min(nums))
+# print("Max:", max(nums))
+# print("Sum:", sum(nums))
+
+# nums.sort()
+# print(nums)
+# animals.sort()
+# print(animals)
+
+# nums.reverse()
+# print(nums)
+# animals.reverse()
+# print(animals)
+
+# print()
+# print("--- Checking Membership ---")
+
+# if "cat" in animals:
+#     print("Cat is in the list!")
+# else:
+#     print("Cat is not in the list.")
+
+# print()
+# print("--- Copying Lists ---")
+
+# original_list = [1, 2, 3]
+# copied_list = original_list.copy()
+# # copied_list = original_list.copy()
+# copied_list.append(4)
+# print(original_list)
+# print(copied_list)
+
+# print()
+# print("--- Nested Lists/The Matrix ---")
+
+# matrix = [  
+#     [1,2,3], 
+#     [4,5,6], 
+#     [7,8,9]  
+#     ] 
+
+# print(matrix[2][2])
+
+# ### **Challenge 1: Animal Swap**
+
+# # Store this list in a variable: [ 1, 2, 3, 4, 5, 6 ] 
+# # Ask the user to enter a new integer.
+# # Replace the **third integer** in the original list with the user’s input, and then print the updated list.
+# # *Hint: use indexing and assignment.*
+
+num = [ 1, 2, 3, 4, 5, 6 ] 
+user_num = int(input("What is a random number? - "))
+new_num = num.index(3)
+num[user_num] = new_num
+print(num)
+
+### **Challenge 2: Shopping List Manager**
+
+# Initialize an empty list called `shopping`.
+# Add three items of your choice using `.append()`.
+# Then insert one extra item at the second position and remove one item of your choice.
+# Finally, print the final shopping list.
+
+shopping = []
+shopping.append("milk")
+shopping.append("eggs")
+shopping.append("bread")
+shopping.insert(2, "orange juice")
+shopping.remove("bread")
+print("Your shopping List:", shopping)
