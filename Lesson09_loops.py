4f9e8aca436d2a5d8d2db9db7f9e81e504807e8c
Shiven <shiven.sharma5111@gmail.com>
2025-10-27 16:45:05 +0000

    msg get better

--- Lesson09_loops.py
+++ Lesson09_loops.py
@@ -1,32 +1,50 @@
-# LOOPS IN PYTHON
-# Loops repeat a block of code until they hit a limit or condition.
-# They exist to save us from typing the same line 500 times.
-# Python gives us for-loops and while-loops.
-# 
+# # LOOPS IN PYTHON
+# # Loops repeat a block of code until they hit a limit or condition.
+# # They exist to save us from typing the same line 500 times.
+# # Python gives us for-loops and while-loops.
+# # 
+# print()
+# print("--- Loops in Python ---")
+
+# # The for-loop.
+# # A for-loop repeats for each element in a sequence (like a list or string).
+# import time 
+
+# animals = ["lamb", "sheep", "cow", "goose", "donkey" ]
+
+# print("\nOur animals:", animals)
+# print("\n--- For Loop: visiting each animal ---")
+
+# # for animal in animals:
+# #     print("Now petting a", animal)
+# #     time.sleep(1.5)
+
+# #     if animal == "sheep":
+# #         print("Hi Shep!")
+
+# print("\nNow I have petted all the animals.")
+
+
+# for i in range(10):
+#     print("Counting:", i)
+
+# # range(start, stop, step)
+# print()
+# print("--- range() with start, stop, step ---")
+
+# for num in range(2, 11, 2):
+#     print("Even number:", num)
+# print("Loop ended. Evens appreciated.")
+
+print("--- Iterating over strings ---\n")
+
+fav_word = "Shenanigan"
+letter_list = []
+
+for letter in fav_word:
+    print(letter, end="")
+    letter_list.append(letter)
+
+print(letter_list)
+
 print()
-print("--- Loops in Python ---")
-
-# The for-loop.
-# A for-loop repeats for each element in a sequence (like a list or string).
-import time 
-
-animals = ["lamb", "sheep", "cow", "goose", "donkey" ]
-
-print("\nOur animals:", animals)
-print("\n--- For Loop: visiting each animal ---")
-
-for animal in animals:
-    print("Now petting a", animal)
-    time.sleep(1.5)
-
-    if animal == "sheep":
-        print("hi shep!")
-
-print("\nNow I have petted all the animals.")
-
-# range with start, stop and step
-
-
-for num in range(2, 11, 2):
-    print("counting", num)
-
